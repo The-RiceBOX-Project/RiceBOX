3ef5d5423f2ca57a382f2bb7cb96350e167fd946
KNProNoob <knpronoob@gmail.com>
2025-02-26 14:22:03 +0200

    Made some changes to the update menu

--- scripts/update_list_item.gd
+++ scripts/update_list_item.gd
@@ -3,6 +3,10 @@ var version
 var date
 var download_url
 @onready var http_req = $HTTPRequest
+@onready var update_text = get_node("../../../UpdatingText")
+@onready var check_for_updates = get_node("../../../Check_for_updates")
+@onready var download_completed = get_node("../../../DownloadCompleted")
+@onready var updates_list = get_node("../../")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,9 +15,11 @@ func _ready() -> void:
 
 
 func _on_update_pressed() -> void:
-	print(download_url)
 	http_req.request(download_url)
-
+	update_text.text = "Downloading version " + version + "..."
+	update_text.show()
+	updates_list.hide()
+	check_for_updates.hide()
 
 func _on_http_request_request_completed(_result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
 	print("aaa")
@@ -23,4 +29,7 @@ func _on_http_request_request_completed(_result: int, response_code: int, _heade
 	var file = FileAccess.open("updates/menu", FileAccess.WRITE)
 	file.store_buffer(body)
 	file.close()
-	print("Update Downloaded")
+	update_text.hide()
+	download_completed.get_node("Label").text = "Update " + version + " has been downloaded, please restart your console in order to apply it."
+	download_completed.show()
+	
