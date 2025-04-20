cd83ad1778f4d71ae6ca379ddd870b85a90045e4
KNProNooob <knpronoob@gmail.com>
2025-04-20 18:03:13 +0300

    The update button in the game details screen now works correctly

--- scripts/game_details.gd
+++ scripts/game_details.gd
@@ -40,16 +40,32 @@ func _on_game_selected(game_path: String, is_on_usb: bool) -> void:
 	
 	if is_on_usb:
 		download_or_update_btn.text = "Download"
-		download_or_update_btn.show()
+		download_or_update_btn.disabled = false
 		delete_btn.hide()
 	else:
-		download_or_update_btn.hide()
+		download_or_update_btn.disabled = true
+		download_or_update_btn.text = "Update"
 		delete_btn.show()
+		check_for_updates()
 	
 	games_list.hide()
 	show()
 	await get_tree().create_timer(0.2).timeout
 	play_btn.grab_focus()
+
+func check_for_updates():
+	var mnt_dir = DirAccess.open(MNT_DIR)
+	var game_on_mnt_path = game_dir_path.lstrip("/" + GAMES_DIR)
+	if mnt_dir.dir_exists(game_on_mnt_path) and mnt_dir.file_exists(game_on_mnt_path + "/manifest.json"):
+		var manifest_file = FileAccess.open(MNT_DIR + "/" + game_on_mnt_path + "/manifest.json", FileAccess.READ)
+		var manifest_json = JSON.parse_string(manifest_file.get_as_text())
+		
+		if manifest_json.version < version_num:
+			download_or_update_btn.text = "Downgrade"
+			download_or_update_btn.disabled = false
+		elif manifest_json.version > version_num:
+			download_or_update_btn.text = "Update"
+			download_or_update_btn.disabled = false
 
 
 func _on_back_pressed() -> void:
@@ -78,5 +94,6 @@ func _on_download_or_update_pressed() -> void:
 	var src = MNT_DIR + game_dir_name
 	var dst = GAMES_DIR + game_dir_name
 	
+	OS.execute("rm", ["-rf", dst])
 	OS.execute("cp", ["-rf", src, dst])
 	_on_back_pressed()
