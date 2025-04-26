957debd16e78604d53f1aa91fbbaa1218805a498
KNProNooob <knpronoob@gmail.com>
2025-04-26 23:55:04 +0300

    The latest update now shows on top

--- Menu/scripts/updates.gd
+++ Menu/scripts/updates.gd
@@ -25,5 +25,4 @@ func _on_fetch_updates_request_completed(_result: int, response_code: int, _head
 			node.date = version.published_at
 			node.download_url = version.assets[0].browser_download_url
 			updates.add_child(node)
-			updates.move_child(node, 0)
 		
