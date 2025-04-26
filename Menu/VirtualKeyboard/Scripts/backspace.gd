93606452fb3e811a1f17ba9c7b41cc087ca76b1e
KNProNooob <knpronoob@gmail.com>
2025-04-27 01:10:09 +0300

    Cleanup

--- Menu/VirtualKeyboard/Scripts/backspace.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-signal clicked(key)
-
-
-
-func _on_pressed() -> void:
-	clicked.emit("backspace")
--- Menu/scripts/poweroff.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_shutdown_pressed() -> void:
-	OS.execute("poweroff", [])
-
-
-func _on_restart_pressed() -> void:
-	OS.execute("reboot", [])
